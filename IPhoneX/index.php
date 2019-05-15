d9db8645b0c25ae4f2fdb33d935b9bb7c6e2d620
ABCode <abcode07@gmail.com>
2019-05-15 16:51:31 +0100

    IPhone-X
    Responsive Landing Product Page

---
+++ IPhoneX/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="icon" type="image/png" href="abb.png">
+	<title> IPhone X </title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" type="text/css" href="responsive.css">
+</head>
+<body>
+
+<div class="container">
+	
+<div class="right_bg"></div>
+
+<div class="des_box">
+	
+<div class="des_box_left_bg"></div>
+
+<div class="image"><img src="iphonex.png"></div>
+
+<div class="des">
+	
+<h2> IPhone X <br> <small><small> Last Edition </small></small> </h2>
+
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+<h3> $1300.45 </h3>
+
+<button> Buy Now </button>
+
+</div>
+
+</div>
+
+</div>
+
+<div class="logo"><img src="apple_logo.png"><h2> APPLE </h2></div>
+
+</body>
+</html>
